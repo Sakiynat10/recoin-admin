1b4e58803eace4b1d4b67ef187ad24b9fd2fefa4
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2024-10-22 12:34:12 +0500

    adding app/(main) app/auth

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,12 @@
+import AuthTabs from '@/components/auth/auth-tab'
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <>
+       <AuthTabs/>
+    </>
+  )
+}
+
+export default AuthPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,15 +34,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}
         storageKey="dashboard-theme"
         >
-        <Navbar/>
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[300px]">
-            <Sidebar/>
-          </div>
-          <div className="p-5 w-full md:max-w-[1140px]">
-            {children}
-          </div>
-        </div>
+          {children}
         </ThemeProvider>
       </body>
     </html>
